f04f5ca63468c674988efe6041ba0b48c297b137
tanvirtareq <cse.tanvir.17@gmail.com>
2025-09-21 00:44:34 +0600

    Add work experience

--- script.js
+++ script.js
@@ -16,12 +16,29 @@ document.addEventListener('DOMContentLoaded', () => {
             data.experience.forEach(item => {
                 const expItem = document.createElement('div');
                 expItem.className = 'experience-item';
+
+                let companyHTML = item.company;
+                if (item.companyWebsite) {
+                    companyHTML += ` <a href="${item.companyWebsite}" target="_blank" rel="noopener noreferrer" class="external-link-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                            <path fill-rule="evenodd" d="M16 0h-5.5a.5.5 0 0 0 0 1h4.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V6.5a.5.5 0 0 0 1 0V.5a.5.5 0 0 0-.5-.5z"/>
+                        </svg>
+                    </a>`;
+                }
+
                 expItem.innerHTML = `
                     <h3>${item.title}</h3>
-                    <p>${item.company} | ${item.period}</p>
+                    <p>${companyHTML} | ${item.period}</p>
                     <ul>
                         ${item.responsibilities.map(res => `<li>${res}</li>`).join('')}
                     </ul>
+                    ${item.technologies && item.technologies.length > 0 ? `
+                        <div class="technologies-used">
+                            <h4>Technologies:</h4>
+                            ${item.technologies.map(tech => `<span>${tech}</span>`).join('')}
+                        </div>
+                    ` : ''}
                 `;
                 experienceContainer.appendChild(expItem);
             });
